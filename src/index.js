d73f2ba56caec1c8513a573acaf48f70d129c698
Chandan Lakkoju <lchandan@outlook.com>
2021-04-18 00:00:03 +0530

    new modifications with github account added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import {Auth0Provider} from '@auth0/auth0-react';
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
+console.log('inside index.js', domain, clientId)
+
 ReactDOM.render(
   <Auth0Provider
     domain={domain}
